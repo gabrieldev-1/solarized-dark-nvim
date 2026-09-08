3ac33624760d5a7e17654a94b146308502bfcb3f
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-09-07 22:19:24 -0300

    change of the color palette to the new standard

--- lua/solarized-dark/colors.lua
+++ lua/solarized-dark/colors.lua
@@ -1,14 +1,20 @@
 return {
-    base01      = "#000000";
-    base02      = "#121212";
-    base03      = "#586E75";
-    base04      = "#A1B3B5";
-    yellow      = "#C49600";
-    orange      = "#D95821";
-    red         = "#E84441";
-    magenta     = "#E24D94";
-    violet      = "#8187DE";
-    blue        = "#329CE8";
-    cyan        = "#34B5AC";
-    green       = "#98AF00";
+    base01      = "#012340";
+    base02      = "#010D00";
+    base03      = "#D9C077";
+    base04      = "#F29F05";
+    base05      = "#BF6B04";
+
+    yellow      = "#D9C077";
+    orange      = "#F29F05";
+    red         = "#E81F0C";
+    magenta     = "#9A64B4";
+    violet      = "#9A64B4";
+    blue        = "#4D9BCB";
+    cyan        = "#5EBAB3";
+    green       = "#85B464";
+
+    text        = "#E8E3D9";
+    comment     = "#756C57";
+    border      = "#3E3A32";
 }
